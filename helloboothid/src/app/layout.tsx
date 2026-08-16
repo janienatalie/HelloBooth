22b83c57393191f817c5edf075962be531f86b0c
janienatalie <janienataliee@gmail.com>
2026-08-16 09:08:40 +0700

    logo 2

--- helloboothid/src/app/layout.tsx
+++ helloboothid/src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "HelloBoothid | Operational System",
   description: "Sistem Manajemen Operasional Terpadu",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
